7086daf9e31de3d16e2eac3c4d3c04ce4bbfceb7
Yuriy Shandor <yuriyshandor@gmail.com>
2018-03-18 21:30:21 +0200

    Add Section Contacts. Mod_02

--- js/script.js
+++ js/script.js
@@ -3,4 +3,25 @@ $(document).ready(function() {
     $(this).toggleClass('toggler-icon_close');
     $('.nav-bar').slideToggle('0.5s');
   });
+
+  // Price Counter
+  var priceCounterStart = 0;
+  $(window).scroll(function() {
+    var oTop = $('.pricing-block').offset().top - window.innerHeight;
+    if (priceCounterStart == 0 && $(window).scrollTop() > oTop) {
+      $('.price-value').each(function () {
+        $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+        },
+        {
+          duration: 3000,
+          easing: 'swing',
+          step: function (now) {
+          $(this).text(Math.ceil(now));
+        }
+        });
+      });
+      priceCounterStart = 1;
+    }
+  });
 });
